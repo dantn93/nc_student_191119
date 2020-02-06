fecdcf7676c2e50cf7bfe6ac5c4388bf38bed25f
Tran Ngoc Dan <dantn93@gmail.com>
2020-02-06 22:55:54 +0700

    delete service and deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-kubectl delete service tranngocdan-nc-student-service
-kubectl delete deployment tranngocdan-nc-student
+# kubectl delete service tranngocdan-nc-student-service
+# kubectl delete deployment tranngocdan-nc-student
 kubectl create -f provision/k8s/deployment.yaml
 kubectl get service tranngocdan-nc-student-service
 
